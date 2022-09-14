d4ebc30fb8c72cfdfd5ea139d57058c02d80da95
Adrianus Vieira <adrianuscardoso@gmail.com>
2022-09-14 09:35:40 -0300

    fix: seting PG_PORT

--- env.ts
+++ env.ts
@@ -16,7 +16,7 @@ import Env from '@ioc:Adonis/Core/Env'
 
 export default Env.rules({
   PG_HOST: Env.schema.string({ format: 'host' }),
-  // PG_PORT: Env.schema.number(),
+  PG_PORT: Env.schema.number(),
   PG_USER: Env.schema.string(),
   PG_PASSWORD: Env.schema.string.optional(),
   PG_DB_NAME: Env.schema.string(),
